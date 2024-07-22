044ace80f1aa9e046c8c47164bd4ad8a5a2af531
Abhishek Sambharya <abhisheksambharya05@gmail.com>
2024-07-22 22:14:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,23 @@ app.use(express.json());
 app.use("/api/v1/users", require("./routes/userRoute"));
 app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 
+// Root route
+app.get('/', (req, res) => {
+    res.send('Welcome to the API!');
+});
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Catch-all route for any other request
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// 404 Not Found
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Route not found" });
+});
 
 // Port
 const PORT = process.env.PORT || 8080;
